c416eff995897d4aa3a0f5507d27c805a9d21aff
Ovidiu Visan <ovisan@MacBook.local>
2026-08-09 11:13:56 +0300

    Migrate nvim-treesitter to main branch API for Neovim 0.12
    Replace removed nvim-treesitter.configs setup with install +
    FileType highlight/indent, and update textobjects to main branch.

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,69 +1,84 @@
+-- nvim-treesitter `main` branch (rewrite for Neovim 0.12+)
+-- Old API: require("nvim-treesitter.configs").setup(...)  -- removed
+-- Docs: https://github.com/nvim-treesitter/nvim-treesitter/tree/main
+
+local ensure_installed = {
+  "bash",
+  "c",
+  "cpp",
+  "css",
+  "diff",
+  "dockerfile",
+  "go",
+  "html",
+  "javascript",
+  "json",
+  "lua",
+  "luadoc",
+  "markdown",
+  "markdown_inline",
+  "python",
+  "query",
+  "regex",
+  "rust",
+  "toml",
+  "tsx",
+  "typescript",
+  "vim",
+  "vimdoc",
+  "yaml",
+}
+
 return {
   {
     "nvim-treesitter/nvim-treesitter",
+    -- Plugin does not support lazy-loading
+    lazy = false,
+    branch = "main",
     build = ":TSUpdate",
-    event = { "BufReadPost", "BufNewFile", "VeryLazy" },
-    cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
-    dependencies = {
-      "nvim-treesitter/nvim-treesitter-textobjects",
-    },
-    opts = {
-      ensure_installed = {
-        "bash",
-        "c",
-        "cpp",
-        "css",
-        "diff",
-        "dockerfile",
-        "go",
-        "html",
-        "javascript",
-        "json",
-        "jsonc",
-        "lua",
-        "luadoc",
-        "markdown",
-        "markdown_inline",
-        "python",
-        "query",
-        "regex",
-        "rust",
-        "toml",
-        "tsx",
-        "typescript",
-        "vim",
-        "vimdoc",
-        "yaml",
-      },
-      auto_install = true,
-      highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = false,
-      },
-      indent = { enable = true },
-      incremental_selection = {
-        enable = true,
-        keymaps = {
-          init_selection = "<C-space>",
-          node_incremental = "<C-space>",
-          scope_incremental = false,
-          node_decremental = "<bs>",
-        },
-      },
-      textobjects = {
+    config = function()
+      local ts = require("nvim-treesitter")
+
+      ts.setup({
+        -- parsers/queries install location (prepended to runtimepath)
+        install_dir = vim.fn.stdpath("data") .. "/site",
+      })
+
+      -- Install missing parsers asynchronously (no-op if already present)
+      ts.install(ensure_installed)
+
+      -- Enable highlight / indent per buffer (not automatic on main)
+      vim.api.nvim_create_autocmd("FileType", {
+        group = vim.api.nvim_create_augroup("TreesitterSetup", { clear = true }),
+        callback = function(event)
+          local buf = event.buf
+          -- Highlight (built into Neovim)
+          pcall(vim.treesitter.start, buf)
+
+          -- Experimental treesitter indent
+          vim.bo[buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+
+          -- Folds (options.lua already sets foldmethod/foldexpr globally;
+          -- reinforce per-window when a TS buffer opens)
+          local win = vim.api.nvim_get_current_win()
+          pcall(function()
+            vim.wo[win][0].foldexpr = "v:lua.vim.treesitter.foldexpr()"
+            vim.wo[win][0].foldmethod = "expr"
+          end)
+        end,
+      })
+    end,
+  },
+
+  {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+    branch = "main",
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
+    event = "VeryLazy",
+    config = function()
+      require("nvim-treesitter-textobjects").setup({
         select = {
-          enable = true,
           lookahead = true,
-          keymaps = {
-            ["af"] = "@function.outer",
-            ["if"] = "@function.inner",
-            ["ac"] = "@class.outer",
-            ["ic"] = { query = "@class.inner", desc = "Select inner class" },
-            ["ab"] = "@block.outer",
-            ["ib"] = "@block.inner",
-            ["aa"] = "@parameter.outer",
-            ["ia"] = "@parameter.inner",
-          },
           selection_modes = {
             ["@parameter.outer"] = "v",
             ["@function.outer"] = "V",
@@ -72,21 +87,43 @@ return {
           include_surrounding_whitespace = false,
         },
         move = {
-          enable = true,
           set_jumps = true,
-          goto_next_start = {
-            ["]f"] = "@function.outer",
-            ["]c"] = "@class.outer",
-          },
-          goto_previous_start = {
-            ["[f"] = "@function.outer",
-            ["[c"] = "@class.outer",
-          },
         },
-      },
-    },
-    config = function(_, opts)
-      require("nvim-treesitter.configs").setup(opts)
+      })
+
+      local select = require("nvim-treesitter-textobjects.select")
+      local move = require("nvim-treesitter-textobjects.move")
+
+      -- Select
+      local select_maps = {
+        ["af"] = "@function.outer",
+        ["if"] = "@function.inner",
+        ["ac"] = "@class.outer",
+        ["ic"] = "@class.inner",
+        ["ab"] = "@block.outer",
+        ["ib"] = "@block.inner",
+        ["aa"] = "@parameter.outer",
+        ["ia"] = "@parameter.inner",
+      }
+      for lhs, query in pairs(select_maps) do
+        vim.keymap.set({ "x", "o" }, lhs, function()
+          select.select_textobject(query, "textobjects")
+        end, { desc = "TS select " .. query })
+      end
+
+      -- Move
+      vim.keymap.set({ "n", "x", "o" }, "]f", function()
+        move.goto_next_start("@function.outer", "textobjects")
+      end, { desc = "Next function start" })
+      vim.keymap.set({ "n", "x", "o" }, "[f", function()
+        move.goto_previous_start("@function.outer", "textobjects")
+      end, { desc = "Prev function start" })
+      vim.keymap.set({ "n", "x", "o" }, "]c", function()
+        move.goto_next_start("@class.outer", "textobjects")
+      end, { desc = "Next class start" })
+      vim.keymap.set({ "n", "x", "o" }, "[c", function()
+        move.goto_previous_start("@class.outer", "textobjects")
+      end, { desc = "Prev class start" })
     end,
   },
 }
